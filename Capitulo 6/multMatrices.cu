8d4fb4480292c7aafd59918913761cde6673cdbb
DanielaFe7GitHub <daniela.milon@ucsp.edu.pe>
2017-06-15 17:04:41 -0500

    Tiempos

--- Capitulo 6/multMatrices.cu
+++ Capitulo 6/multMatrices.cu
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <time.h>
 
+using namespace std;
 __global__
 void matMultKernel(float *d_M, float *d_N, float *d_P, int Width){
   int Row = blockIdx.y*blockDim.y + threadIdx.y;
@@ -25,8 +26,8 @@ void matMult(float* A, float* B, float* C, int n){
   cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
   cudaMalloc((void **) &d_C, size);
 
-  dim3 dimGrid(ceil(n/8.0),ceil(n/16.0),1);
-  dim3 dimBlock(8,16,1);  /*Dimensiónde 2 X 2, eje Z desactivado con 1*/
+  dim3 dimGrid(ceil(n/1024.0),ceil(n/1024.0),1);
+  dim3 dimBlock(1024,1024,1);  /*Dimensiónde 2 X 2, eje Z desactivado con 1*/
   matMultKernel<<<dimGrid, dimBlock>>>(d_A,d_B,d_C,n);
   
   cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
@@ -38,27 +39,34 @@ void matMult(float* A, float* B, float* C, int n){
 int main(){
   int n,i,j;
   float *h_A,*h_B,*h_C;
-  n = 5; //Matrices cuadradas de 5 X 5
-  h_A = (float*) malloc(n*n*sizeof(float));
-  h_B = (float*) malloc(n*n*sizeof(float));
-  h_C = (float*) malloc(n*n*sizeof(float));
+  n = 10000;
+  /******************Time Variables*********************************/
+  clock_t time;
+  cout<<"El Tam de la matriz Cuadrada es: "<<n<<" X "<<n<<endl;
+  h_A = new float[n*n];
+  h_B = new float[n*n];
+  h_C = new float[n*n];
+
   for(i = 0; i < n; i++){
-    //scanf("%f", &h_A[i]);
     for(j = 0; j < n; j++)
-      h_A[i*n+j] = rand() % 100;
+      h_A[i*n+j] = 1;
   }
   for(i = 0; i < n; i++){
-    //scanf("%f", &h_B[i]);
     for(j = 0; j < n; j++)
-      h_B[i*n+j] = rand() % 100;
+      h_B[i*n+j] = 1;
   }
+  time = clock();
   matMult(h_A,h_B,h_C,n);
-  for(i = 0; i < n; i++){
+  time = clock() - time;
+  cout<<"El Tiempo  es: "<<(((float)time)/CLOCKS_PER_SEC)<<endl;
+
+
+  /*for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
-        printf("%f ", h_C[i*n+j]);
+        cout<<h_C[i*n+j]<<" ; ";
     }
-    printf("\n");	
+    cout<<endl;
   }
-  printf("\n");
+    cout<<endl;*/
   return 0;
 }
